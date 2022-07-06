99e0225fa9bbc3bfd0232e45f17f8e5129a13fec
Loic Robin <loic.robin@epsi.fr>
2022-07-06 15:25:37 +0200

    modif script

--- NouvelleBase/requete_simple.sql
+++ NouvelleBase/requete_simple.sql
@@ -1,11 +1,16 @@
 -- Chercher les demandes qui ont été faites après une date donnée:
-SELECT * FROM DEMANDE WHERE DATEDEMANDE <= DATE '2021-12-31';
+SELECT * FROM DEMANDE WHERE DATEDEMANDE >= DATE '2017-12-31';
 -- validée
 
 -- Pour une demande donnée, afficher la raison sociale de l’entreprise, la tournée correspondante et la quantité à récupérer pour chaque type de déchet:
-SELECT ENTREPRISE.RAISONSOCIALE,
-FROM DEMANDE INNER JOIN ENTREPRISE ON ENTREPRISE.SIRET = DEMANDE.SIRET
-WHERE DEMANDE.NODEMANDE = 1;
+SELECT b.RAISONSOCIALE, c.NOTOURNEE, e.NOMTYPEDECHET, sum(d.QUANTITEENLEVEE) as QuantiteeRecup
+FROM DEMANDE a INNER JOIN ENTREPRISE b ON a.SIRET = b.SIRET
+INNER JOIN TOURNEE c ON a.NOTOURNEE = c.NOTOURNEE
+INNER JOIN DETAILDEMANDE d ON a.NODEMANDE = d.NODEMANDE
+INNER JOIN TYPEDECHET e ON d.NOTYPEDECHET = e.NOTYPEDECHET
+WHERE a.NODEMANDE = 1 -- demande donnée
+GROUP BY  b.RAISONSOCIALE, c.NOTOURNEE, e.NOMTYPEDECHET;
+-- corrigée
 
 -- Afficher la quantité totale récupérée par type de déchet pour un mois/année donné:
 SELECT TYPEDECHET.NOMTYPEDECHET, SUM(DETAILDEMANDE.QUANTITEENLEVEE) as SommeQuantite
@@ -13,7 +18,7 @@ FROM DETAILDEMANDE INNER JOIN DEMANDE ON DEMANDE.NODEMANDE = DETAILDEMANDE.NODEM
 INNER JOIN TYPEDECHET ON TYPEDECHET.NOTYPEDECHET = DETAILDEMANDE.NOTYPEDECHET
 WHERE DEMANDE.DATEDEMANDE >= DATE '2018-01-01' AND DATEDEMANDE <= DATE '2018-12-31'
 GROUP BY TYPEDECHET.NOMTYPEDECHET;
-
+-- corrigée
 
 -- Afficher les employés ayant réalisé moins de n tournées. Triez le résultat sur le nombre de tournées. N étant un nombre de votre choix
 SELECT *, COUNT(NOTOURNEE) as nbtournees
@@ -24,20 +29,33 @@ ORDER BY nbtournees DESC;
 SELECT EMPLOYE.NOEMPLOYE, EMPLOYE.NOM, EMPLOYE.PRENOM, COUNT(TOURNEE.NOTOURNEE) as nbtournees
 FROM EMPLOYE INNER JOIN TOURNEE ON EMPLOYE.NOEMPLOYE = TOURNEE.NOEMPLOYE
 GROUP BY EMPLOYE.NOEMPLOYE, EMPLOYE.NOM, EMPLOYE.PRENOM
-HAVING COUNT(TOURNEE.NOTOURNEE) < 10
+HAVING COUNT(TOURNEE.NOTOURNEE) < 10 -- n = 10
 ORDER BY nbtournees DESC;
 
+--Affichez les informations de l’entreprise qui a réalisé plus de demandes que l’entreprise Formalys (ouune autre entreprise dont vous fournissez le nom).
+SELECT b.RAISONSOCIALE, COUNT(a.NODEMANDE) as NumDemande
+FROM DEMANDE a INNER JOIN ENTREPRISE b ON a.SIRET = b.SIRET
+GROUP BY b.RAISONSOCIALE
+HAVING COUNT(a.NODEMANDE) > (SELECT COUNT(NODEMANDE) FROM DEMANDE INNER JOIN ENTREPRISE ON DEMANDE.SIRET = ENTREPRISE.SIRET WHERE RAISONSOCIALE = 'Formalys')
+ORDER BY NumDemande desc;
 
 --Affichez les informations de l’entreprise qui a réalisé plus de demandes que l’entreprise Formalys (ouune autre entreprise dont vous fournissez le nom).
-SELECT a.RAISONSOCIALE, COUNT(b.NODEMANDE) as nbdemande
-FROM ENTREPRISE a INNER JOIN DEMANDE b ON a.SIRET = b.SIRET
-WHERE nbdemande > (SELECT COUNT(*) FROM ENTREPRISE a INNER JOIN DEMANDE b ON a.SIRET = b.SIRET WHERE a.RAISONSOCIALE = 'Formalys');
+CREATE OR REPLACE FUNCTION f_nb_demande_superieure_a(raison_sociale VARCHAR2) RETURN VARCHAR2 IS
+    entr_avec_plus_de_demande VARCHAR2(50);
+BEGIN
+    SELECT RAISONSOCIALE INTO entr_avec_plus_de_demande
+    FROM DEMANDE d INNER JOIN ENTREPRISE e ON d.SIRET = e.SIRET
+    GROUP BY RAISONSOCIALE
+    HAVING COUNT(d.NODEMANDE) > (SELECT COUNT(NODEMANDE) FROM DEMANDE d INNER JOIN ENTREPRISE e ON d.SIRET = e.SIRET WHERE RAISONSOCIALE = raison_sociale)
+    ORDER BY COUNT(NODEMANDE) DESC
+    FETCH FIRST 1 ROW ONLY;
 
---• Affichez les informations des demandes qui ne sont pas encore inscrites dans une tournée
-SELECT *, COUNT(NOTOURNEE) as nbtournee FROM DEMANDE
-INNER JOIN TOURNEE ON DEMANDE.NOTOURNEE = TOURNEE.NOTOURNEE
-WHERE nbtournee = 0
+    return entr_avec_plus_de_demande;
+END;
+/
+SELECT f_nb_demande_superieure_a('Formalys') FROM DUAL;
 
---2ème solution
+-- Affichez les informations des demandes qui ne sont pas encore inscrites dans une tournée
+-- OK
 SELECT * FROM DEMANDE
 WHERE NOTOURNEE IS NULL;
--- PL-SQL/script_plsql.sql
+++ PL-SQL/script_plsql.sql
@@ -1,15 +1,19 @@
 alter session set "_ORACLE_SCRIPT"=true;
-SET SERVEROUTPUT ON;
+SET SERVEROUTPUT ON
 
 /* requete 1 */
 CREATE OR REPLACE PROCEDURE totalDechet (dateDeb in DATE, dateFin in DATE, numSite in INTEGER, typeDechet in VARCHAR) as
 totalDechet int;
     BEGIN
-    select sum(quantitedepot) into totaldechet from detaildepot,typedechet,tournee
+    select sum(quantitedepot) into totaldechet from detaildepot, typedechet,tournee,SITE,EMPLOYE
     where typedechet.notypedechet = detaildepot.notypedechet
+    and site.nosite = employe.nosite
+    and employe.noemploye = tournee.noemploye
     and tournee.notournee = detaildepot.notournee
     and typedechet.nomtypedechet = typeDechet
+    and site.nosite = numSite
     and tournee.datetournee BETWEEN datedeb AND datefin;
+    RETURN totaldechet;
     DBMS_OUTPUT.PUT_LINE(totaldechet);
     END;
 /
@@ -19,31 +23,40 @@ call totaldechet('10/09/18','21/10/18',1,'Papier');
 
 /* requete 2 */
 CREATE OR REPLACE PROCEDURE totalDechetNoSite (dateDeb in DATE, dateFin in DATE, typeDechet in VARCHAR) as
-totalDechet int;
+BEGIN
+DECLARE
+typeDechet int;
     BEGIN
-    select sum(quantitedepot) into totaldechet from detaildepot,typedechet,tournee
-    where typedechet.notypedechet = detaildepot.notypedechet
-    and tournee.notournee = detaildepot.notournee
-    and typedechet.nomtypedechet = typeDechet
-    and tournee.datetournee BETWEEN datedeb AND datefin;
-    DBMS_OUTPUT.PUT_LINE(totaldechet);
+    SELECT SUM(QUANTITEENLEVEE) into typeDechet
+    FROM DETAILDEMANDE
+    INNER JOIN DEMANDE ON DEMANDE.NODEMANDE = DETAILDEMANDE.NODEMANDE
+    WHERE DETAILDEMANDE.NOTYPEDECHET LIKE typeDechet
+    AND DEMANDE.DATEENLEVEMENT BETWEEN dateDeb AND dateFin;
+    return typeDechet;
     END;
+  END;
 /
-commit;
 
+call totalDechetNoSite('10/09/18','21/10/18','Papier');
 
+/* requete 3 */
 
 CREATE OR REPLACE PROCEDURE updateNullTournee(numDemande NUMBER, numTournee NUMBER)
 IS
 BEGIN
-   UPDATE DEMANDE SET NOTOURNEE = numTournee where NODEMANDE=numDemande;
+    DBMS_OUTPUT.PUT_LINE('je suis sur le point');
+   UPDATE demande t SET t.NOTOURNEE = numTournee WHERE t.NODEMANDE = numDemande;
 END;
+/
 
 CREATE OR REPLACE PROCEDURE insertNullTourneeInJournal(numDemande NUMBER)
 IS
 BEGIN
-   INSERT INTO JOURNALDEMANDE VALUES (numDemande);
+   INSERT INTO demandeatraiter (NOTOURNEE) VALUES (numDemande);
 END;
+/
+
+
 
 CREATE OR REPLACE FUNCTION checkIfDateAvailable(dateToCheck DATE)
 RETURN INT
@@ -61,22 +74,45 @@ BEGIN
    end if;
    RETURN numTournee;
 END;
+/
+
 
 DECLARE
     CURSOR c_fournisseur IS
-        SELECT *
+        SELECT NODEMANDE, DATEENLEVEMENT
         FROM Demande
         WHERE NOTOURNEE IS NULL;
     numTournee INTEGER;
     BEGIN
-    OPEN c_fournisseur;
         FOR v_fournisseur IN c_fournisseur LOOP
-            numTournee := checkIfDateAvailable(v_fournisseur.DATEENLEVEMENT);
-            IF numTournee IS NOT NULL
-                THEN
-                updateNullTournee(v_fournisseur.NODEMANDE);
-            ELSE insertNullTourneeInJournal(v_fournisseur.NODEMANDE, v_fournisseur.NOTOURNEE);
+           SELECT checkIfDateAvailable(v_fournisseur.DATEENLEVEMENT) INTO numTournee FROM DUAL;
+            IF numTournee IS NOT NULL THEN
+                updateNullTournee(v_fournisseur.NODEMANDE, numTournee);
+            ELSE
+                insertNullTourneeInJournal(v_fournisseur.NODEMANDE);
             end if;
         END LOOP;
-    CLOSE c_fournisseur;
-    END;
+END;
+/
+
+
+
+/* requete 4 */
+CREATE OR REPLACE TRIGGER verificationTotalDechet
+BEFORE INSERT on detaildepot
+FOR EACH ROW
+DECLARE
+quantiteCollecter integer;
+BEGIN
+    SELECT SUM(d.quantiteenlevee) as total_qte INTO quantiteCollecter
+    FROM DETAILDEPOT a INNER JOIN TOURNEE b ON a.NOTOURNEE = b.notournee
+    INNER JOIN demande c ON b.NOTOURNEE = c.notournee
+    INNER JOIN DETAILDEMANDE d ON c.nodemande = d.nodemande
+    WHERE a.notournee = :new.NOTOURNEE;
+    if quantiteCollecter < :new.QUANTITEDEPOT THEN
+        RAISE_APPLICATION_ERROR(-20010, 'La quantite dépose est supérieure à la quantité collecter');
+    end if;
+END;
+/
+
+insert into detaildepot(notournee, nocentre,notypedechet,quantitedepot) values (1,1,7,500000);
